9246c4e42e5e5c3c2e1f7aeb234aae88830c1a4a
jmalagonn <nicolas0915@hotmail.com>
2020-05-13 18:45:55 -0500

    Favicon

--- views/head.php
+++ views/head.php
@@ -6,5 +6,6 @@
     <title>Endeavor Mentors' Live</title>
     <link rel="stylesheet" href="<?php echo constant('URL') ?>public/styles/bootstrap-4.5.0/bootstrap.css">
     <link rel="stylesheet" href="<?php echo constant('URL') ?>public/styles/style.css">
+    <link rel="shortcut icon" href="<?php echo constant('URL') ?>/public/images/e.png" type="image/x-icon">
 </head>
 <body>
